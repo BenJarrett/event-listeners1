8ad829e517e0e6459a9b866e50cc64739ef81be8
Ben Jarrett <b.cl.jarrett@gmail.com>
2021-01-23 11:08:40 -0600

    working with class callbacks

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("This is for event-listeners")
+// console.log("This is for event-listeners")
+
+funcion firstOne (arg) {
+  return `Hello ${arg}!`;
+}
+
+function firstOneTwo (arg) {
+  return `GoodbyeBen ${arg}!`;
+}
+
+function createGreeting (arg, cb) {
+  return cb(arg);
+
+}
+
+console.log(createGreeting('Ben', firstOne)); 
+console.log(createGreeting('Ben', firstOneTwo));
